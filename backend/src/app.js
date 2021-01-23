830e278c922a2cda55878f6d6db478fba95868ff
Reekraj Roy <55712612+REEKRAJROY@users.noreply.github.com>
2021-01-24 02:56:51 +0530

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const hbs = require("hbs");
+require("./db/conn");
+const port = process.env.PORT || 3000;
+
+const static_path = path.join(_dirname, "../public");
+const template_path = path.join(_dirname, "../templates/views");
+const template_path = path.join(_dirname, "../templates/partials");
+
+
+//console.log(path.join(_dirname, "../public"));
+app.use(express.static(static_path));
+app.set("view engine", "hbs");
+app.set("views", template_path);
+hbs.registerPartials(partials_path);
+
+app.get("/", (req, res) =>{
+    res.render("index")
+});
+
+app.listen(port, () => {
+    console.log(`server is running at port no ${port}`);
+})
